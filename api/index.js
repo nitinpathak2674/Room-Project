57f4f388ee35af175f803e92bfa8193ed40af45a
Nitin Pathak <pathakn475@example.com>
2026-04-08 09:22:14 +0530

    clean backend code

--- api/index.js
+++ api/index.js
@@ -1,33 +1,26 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+
 const authRoutes = require('./routes/authRoutes');
 const bookingRoutes = require('./routes/bookingRoutes');
-const db = require('./config/db');
+const roomRoutes = require('./routes/roomRoutes');
 
 const app = express();
 
-// CORS ko thoda flexible rakho deployment ke liye
+// Middlewares
 app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use('https://room-reserve-clean.onrender.com/api/auth', authRoutes);
-app.use('https://room-reserve-clean.onrender.com/api/bookings', bookingRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/rooms", roomRoutes);
+app.use("/api/bookings", bookingRoutes);
 
-// Test Route
-app.get('https://room-reserve-clean.onrender.com/api/rooms', async (req, res) => {
-    try {
-        const [rows] = await db.query('SELECT * FROM rooms');
-        res.json(rows);
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
+// Root check
+app.get("/", (req, res) => {
+  res.send("Server is running perfectly!");
 });
-
-
-app.get("/", (req, res) => res.send("Server is running perfectly!"));
-
 
 const PORT = process.env.PORT || 5000;
 
